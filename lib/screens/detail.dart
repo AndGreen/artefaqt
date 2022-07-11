3bb528acc267f4943f860cc39befa5c0d94ff2f8
zelenin.andrew <zelenin.andrew@gmail.com>
2022-07-11 19:08:47 +0300

    added dino svg on detail page

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,7 +1,7 @@
 import 'package:artefaqt/components/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:rive/rive.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../models/item.dart';
 
@@ -21,14 +21,6 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
-  SMITrigger? _controller;
-
-  void _onRiveInit(Artboard artboard) {
-    final controller = StateMachineController.fromArtboard(artboard, 'Motion');
-    artboard.addController(controller!);
-    _controller = controller.findInput<bool>('Skin') as SMITrigger;
-  }
-
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)?.settings.arguments as DetailArguments;
@@ -37,51 +29,64 @@ class _DetailScreenState extends State<DetailScreen> {
         appBar: CustomAppBar(
           title: args.item.title,
         ),
-        body: GestureDetector(
-          onTap: () {
-            setState(() {
-              _controller?.fire();
-            });
-          },
-          child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: RatingBar.builder(
-                initialRating: args.item.rating,
-                minRating: 1,
-                glow: false,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                ignoreGestures: true,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                itemBuilder: (context, _) => const Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-                onRatingUpdate: (rating) {},
-              ),
+        body: Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: SvgPicture.asset(height: 250, 'assets/dino_likes.svg'),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(minHeight: 250),
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: Center(
-                        child: args.item.comment.isNotEmpty
-                            ? Text(args.item.comment,
-                                style: const TextStyle(fontSize: 16))
-                            : const Text(
-                                '🤷‍♂️',
-                                style: TextStyle(fontSize: 30),
-                              )),
+            Column(children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: RatingBar.builder(
+                  initialRating: args.item.rating,
+                  minRating: 1,
+                  glow: false,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  ignoreGestures: true,
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxHeight: 350),
+                  child: Card(
+                    child: MediaQuery.removePadding(
+                      context: context,
+                      removeTop: true,
+                      removeBottom: true,
+                      child: Center(
+                        child: Scrollbar(
+                          child: SingleChildScrollView(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Center(
+                                  child: args.item.comment.isNotEmpty
+                                      ? Text(args.item.comment,
+                                          style: const TextStyle(fontSize: 16))
+                                      : const Text(
+                                          '🤷‍♂️',
+                                          style: TextStyle(fontSize: 30),
+                                        )),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ]),
+            ]),
+          ],
         ));
   }
 }
